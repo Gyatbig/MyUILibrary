97ce7dfacf9bc604e10296dff5eb4d2497dfaab5
Gyatbig <atharfathan664@gmail.com>
2025-03-29 14:54:04 +0300

    allscriptxyznothing.lua

--- allscriptxyznothing.lua
+++ allscriptxyznothing.lua
@@ -5,77 +5,39 @@ local gui = Library:create{
 }
 
 local BLOXFFRUITTAB = gui:tab{
-    Icon = "rbxassetiad://6034996695",
+    Icon = "rbxassetid://6034996695",
     Name = "BLOXFRUIT-TABS"
 }
 
 BLOXFFRUITTAB:button({
     Name = "REDZ-HUB",
     Callback = function()
-    loadstring(game:HttpGet("https://pastefy.app/ACOX6D6h/raw"))()
-
+        loadstring(game:HttpGet("https://pastefy.app/ACOX6D6h/raw"))()
     end,
 })
 BLOXFFRUITTAB:button({
     Name = "WINNIX-HUB",
     Callback = function()
-    getgenv().Team = "Pirates"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/NxNDemon/Winnix-trash/refs/heads/main/humm.txt"))()
-    end,})
+        getgenv().Team = "Pirates"
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/NxNDemon/Winnix-trash/refs/heads/main/humm.txt"))()
+    end,
+})
 BLOXFFRUITTAB:button({
     Name = "TSUO-HUB",
     Callback = function()
-    getgenv().Team = "Pirates"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tsuo7/TsuoHub/main/Tsuoscripts"))()
-    end,})
+        getgenv().Team = "Pirates"
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Tsuo7/TsuoHub/main/Tsuoscripts"))()
+    end,
+})
 BLOXFFRUITTAB:button({
     Name = "ASTRAL-HUB",
     Callback = function()
-    getgenv().Team = "Pirates"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
-    end,})
-BLOXFFRUITTAB:button({
-    Name = "ASTRAL-HUB",
-    Callback = function()
-    getgenv().Team = "Pirates"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
-    end,})
-BLOXFFRUITTAB:button({
-    Name = "ASTRAL-HUB",
-    Callback = function()
-    getgenv().Team = "Pirates"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
-    end,})
-BLOXFFRUITTAB:button({
-    Name = "ASTRAL-HUB",
-    Callback = function()
-    getgenv().Team = "Pirates"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
-    end,})
-BLOXFFRUITTAB:button({
-    Name = "ASTRAL-HUB",
-    Callback = function()
-    getgenv().Team = "Pirates"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
-    end,})
-BLOXFFRUITTAB:button({
-    Name = "ASTRAL-HUB",
-    Callback = function()
-    getgenv().Team = "Pirates"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
-    end,})
-BLOXFFRUITTAB:button({
-    Name = "ASTRAL-HUB",
-    Callback = function()
-    getgenv().Team = "Pirates"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
-    end,})
-BLOXFFRUITTAB:button({
-    Name = "ASTRAL-HUB",
-    Callback = function()
-    getgenv().Team = "Pirates"
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
-    end,})
+        getgenv().Team = "Pirates"
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
+    end,
+})
+
+-- Additional button definitions follow...
 
 local DEADRAILS = gui:tab{
     Icon = "rbxassetid://95777119333912",
@@ -85,8 +47,9 @@ local DEADRAILS = gui:tab{
 DEADRAILS:button({
     Name = "MARKK-HUB",
     Callback = function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoGunVolley/Dedrail/refs/heads/main/Protected_8860927939362090.txt"))()
-    end,})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoGunVolley/Dedrail/refs/heads/main/Protected_8860927939362090.txt"))()
+    end,
+})
 
 local cdid = gui:tab{
     Icon = "rbxassetid://95777119333912",
@@ -95,15 +58,17 @@ local cdid = gui:tab{
 
 cdid:button({
     Name = "ATOMICAL",
-	Description = "clicked the below button, complete the key system first, and press this button then paste it to your executor!",
+    Description = "clicked the below button, complete the key system first, and press this button then paste it to your executor!",
     Callback = function()
-    setclipboard('loadstring(game:HttpGet("https://getsades.net/Module/FreeAtomic.lua"))()')        
-    end,})
+        setclipboard('loadstring(game:HttpGet("https://getsades.net/Module/FreeAtomic.lua"))()')        
+    end,
+})
 cdid:button({
     Name = "ATOMICAL KEY LINK",
     Callback = function()
-    setclipboard('https://ads.luarmor.net/get_key?for=Atomicals_Free-omqpOcYrZyJL')        
-    end,})
+        setclipboard('https://ads.luarmor.net/get_key?for=Atomicals_Free-omqpOcYrZyJL')        
+    end,
+})
 
 local EXTRAFUNTABS = gui:tab{
     Icon = "rbxassetid://95777119333912",
@@ -112,25 +77,32 @@ local EXTRAFUNTABS = gui:tab{
 
 EXTRAFUNTABS:button({
     Name = "infinity-yield",
-	Description = "fun tool to use",
+    Description = "fun tool to use",
     Callback = function()
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()    
-    end,}
-EXTRAFUNTABS:button({
-    Name = "infinity-yield",
-	Description = "fun tool to use",
-    Callback = function()
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()    
-    end,}
+        loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'), true))()
+    end,
+})
+
 EXTRAFUNTABS:button({
     Name = "chatbot",
-	Description = "fun tool to use",
+    Description = "fun tool to use",
     Callback = function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/ZbzbLztS"))()    
-    end,}
+        loadstring(game:HttpGet("https://pastebin.com/raw/ZbzbLztS"))()    
+    end,
+})
+
 EXTRAFUNTABS:button({
     Name = "SUS",
-	Description = "fun tool to use",
+    Description = "fun tool to use",
     Callback = function()
-    loadstring(game:HttpGet("https://pastebin.com/raw/Pv0rqM2h"))()    
-    end,}
+        loadstring(game:HttpGet("https://pastebin.com/raw/Pv0rqM2h"))()    
+    end,
+})
+
+EXTRAFUNTABS:button({
+    Name = "SUSv2",
+    Description = "fun tool to use",
+    Callback = function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/FWwdST5Y"))()    
+    end,
+})
