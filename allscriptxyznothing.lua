3ba995db78afaa6467b76e5aa60d86fddafa923a
Gyatbig <atharfathan664@gmail.com>
2025-03-29 13:19:28 +0300

    allscriptxyznothing.lua

---
+++ allscriptxyznothing.lua
@@ -0,0 +1,89 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+
+local gui = Library:create{
+    Theme = Library.Themes.Serika
+}
+
+local BLOXFFRUITTAB = gui:tab{
+    Icon = "rbxassetid://6034996695",
+    Name = "BLOXFRUIT-TABS"
+}
+
+BLOXFFRUITTAB:button({
+    Name = "REDZ-HUB",
+    Callback = function()
+    loadstring(game:HttpGet("https://pastefy.app/ACOX6D6h/raw"))()
+
+    end,
+})
+BLOXFFRUITTAB:button({
+    Name = "WINNIX-HUB",
+    Callback = function()
+    getgenv().Team = "Pirates"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NxNDemon/Winnix-trash/refs/heads/main/humm.txt"))()
+    end,})
+BLOXFFRUITTAB:button({
+    Name = "TSUO-HUB",
+    Callback = function()
+    getgenv().Team = "Pirates"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Tsuo7/TsuoHub/main/Tsuoscripts"))()
+    end,})
+BLOXFFRUITTAB:button({
+    Name = "ASTRAL-HUB",
+    Callback = function()
+    getgenv().Team = "Pirates"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
+    end,})
+BLOXFFRUITTAB:button({
+    Name = "ASTRAL-HUB",
+    Callback = function()
+    getgenv().Team = "Pirates"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
+    end,})
+BLOXFFRUITTAB:button({
+    Name = "ASTRAL-HUB",
+    Callback = function()
+    getgenv().Team = "Pirates"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
+    end,})
+BLOXFFRUITTAB:button({
+    Name = "ASTRAL-HUB",
+    Callback = function()
+    getgenv().Team = "Pirates"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
+    end,})
+BLOXFFRUITTAB:button({
+    Name = "ASTRAL-HUB",
+    Callback = function()
+    getgenv().Team = "Pirates"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
+    end,})
+BLOXFFRUITTAB:button({
+    Name = "ASTRAL-HUB",
+    Callback = function()
+    getgenv().Team = "Pirates"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
+    end,})
+BLOXFFRUITTAB:button({
+    Name = "ASTRAL-HUB",
+    Callback = function()
+    getgenv().Team = "Pirates"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
+    end,})
+BLOXFFRUITTAB:button({
+    Name = "ASTRAL-HUB",
+    Callback = function()
+    getgenv().Team = "Pirates"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Overgustx2/Main/refs/heads/main/BloxFruits_25.html"))()
+    end,})
+
+local DEADRAILS = gui:tab{
+    Icon = "rbxassetid://95777119333912",
+    Name = "DEADRIALS-TABS"
+}
+
+DEADRAILS:button({
+    Name = "MARKK-HUB",
+    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoGunVolley/Dedrail/refs/heads/main/Protected_8860927939362090.txt"))()
+    end,})
