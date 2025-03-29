4c5fa4f80d2694312485892c6fd7cf97d04cffc8
Gyatbig <atharfathan664@gmail.com>
2025-03-29 14:41:42 +0300

    allscriptxyznothing.lua

--- allscriptxyznothing.lua
+++ allscriptxyznothing.lua
@@ -5,7 +5,7 @@ local gui = Library:create{
 }
 
 local BLOXFFRUITTAB = gui:tab{
-    Icon = "rbxassetid://6034996695",
+    Icon = "rbxassetiad://6034996695",
     Name = "BLOXFRUIT-TABS"
 }
 
@@ -87,3 +87,50 @@ DEADRAILS:button({
     Callback = function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TwoGunVolley/Dedrail/refs/heads/main/Protected_8860927939362090.txt"))()
     end,})
+
+local cdid = gui:tab{
+    Icon = "rbxassetid://95777119333912",
+    Name = "CDID-TABS"
+}
+
+cdid:button({
+    Name = "ATOMICAL",
+	Description = "clicked the below button, complete the key system first, and press this button then paste it to your executor!",
+    Callback = function()
+    setclipboard('loadstring(game:HttpGet("https://getsades.net/Module/FreeAtomic.lua"))()')        
+    end,})
+cdid:button({
+    Name = "ATOMICAL KEY LINK",
+    Callback = function()
+    setclipboard('https://ads.luarmor.net/get_key?for=Atomicals_Free-omqpOcYrZyJL')        
+    end,})
+
+local EXTRAFUNTABS = gui:tab{
+    Icon = "rbxassetid://95777119333912",
+    Name = "EXTRAFUN-TABS"
+}
+
+EXTRAFUNTABS:button({
+    Name = "infinity-yield",
+	Description = "fun tool to use",
+    Callback = function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()    
+    end,}
+EXTRAFUNTABS:button({
+    Name = "infinity-yield",
+	Description = "fun tool to use",
+    Callback = function()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()    
+    end,}
+EXTRAFUNTABS:button({
+    Name = "chatbot",
+	Description = "fun tool to use",
+    Callback = function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/ZbzbLztS"))()    
+    end,}
+EXTRAFUNTABS:button({
+    Name = "SUS",
+	Description = "fun tool to use",
+    Callback = function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/Pv0rqM2h"))()    
+    end,}
